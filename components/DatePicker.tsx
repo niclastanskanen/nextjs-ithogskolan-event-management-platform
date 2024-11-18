65b26ddf4a8fb947052f4eff9b752dc3be144969
Niclas <niclastanskanen@hotmail.com>
2024-11-18 11:30:50 +0100

    add DatePicker component with calendar

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,5 +1,22 @@
+import { CalendarIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { Calendar } from "@/components/ui/calendar";
+
 const DatePicker = () => {
-  return <div>DatePicker</div>;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button>
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          <span>Pick a date</span>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-9" align="start">
+        <Calendar mode="single" initialFocus />
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default DatePicker;
