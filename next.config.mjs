3b68892212317f5a2e360ceef8ccb7820aa544b3
Niclas <niclastanskanen@hotmail.com>
2024-11-19 06:19:54 +0100

    add image remote patterns to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s1.ticketm.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.universe.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
