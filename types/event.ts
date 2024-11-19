55a0bc811c188ea770f0458821f4e2233d6d15ee
Niclas <niclastanskanen@hotmail.com>
2024-11-19 15:01:15 +0100

    add interface EventDetails to types

--- types/event.ts
+++ types/event.ts
@@ -16,3 +16,56 @@ export interface Event {
     currency: string;
   };
 }
+
+export interface EventDetails {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  location: string;
+  image?: string;
+  venue?: {
+    name: string;
+    address: {
+      line1?: string;
+      line2?: string;
+      city?: string;
+      state?: string;
+      postalCode?: string;
+      country?: string;
+    };
+    location?: {
+      latitude?: string;
+      longitude?: string;
+    };
+    generalInfo?: string;
+    parkingInfo?: string;
+  };
+  startDate?: string;
+  startTime?: string;
+  status?: string;
+  timezone?: string;
+  priceRanges?: Array<{
+    type: string;
+    currency: string;
+    min: number;
+    max: number;
+  }>;
+  ticketLimit?: string;
+  ageRestrictions?: string;
+  doorTime?: string;
+  pleaseNote?: string;
+  accessibility?: string;
+  ticketingInfo: {
+    url: string;
+    onSaleStatus?: string;
+    onSaleStartDate?: string;
+    onSaleEndDate?: string;
+  };
+  seatmap?: string;
+  organizer: {
+    name: string;
+    description?: string;
+    email: string;
+  };
+}
