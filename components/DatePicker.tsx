e94514040f0a7e021bb61b5da89b4dae4c5a2b79
Niclas <niclastanskanen@hotmail.com>
2024-11-19 07:14:50 +0100

    refactor DatePicker component to handle selected date and onSelect

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,19 +1,43 @@
 import { CalendarIcon } from "lucide-react";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-import { Button } from "./ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
 
-const DatePicker = () => {
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface DatePickerProps {
+  date?: Date;
+  onSelect?: (date?: Date) => void;
+}
+
+const DatePicker = ({ date, onSelect }: DatePickerProps) => {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[240px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          <span>Pick a date</span>
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-9" align="start">
-        <Calendar mode="single" initialFocus />
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={onSelect}
+          initialFocus
+        />
       </PopoverContent>
     </Popover>
   );
