43399dcc9d82e1709f625d11ce30e1cfafd3e496
Niclas <niclastanskanen@hotmail.com>
2024-11-19 06:46:42 +0100

    add formatDate in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { format, parseISO } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString: string): string {
+  try {
+    const date = parseISO(dateString);
+    return format(date, "PPP");
+  } catch (error) {
+    return dateString;
+  }
 }
